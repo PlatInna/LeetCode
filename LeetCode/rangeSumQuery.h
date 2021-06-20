48b37388c80fb17efce1ac6f593a0e8ba32b7d5a
Plat <yarkinpy@gmail.com>
2021-06-19 21:05:06 -0500

    Segment Tree

--- LeetCode/rangeSumQuery.h
+++ LeetCode/rangeSumQuery.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 /*
 Given an integer array nums, handle multiple queries of the following types:
     - Update the value of an element in nums.
@@ -23,53 +23,65 @@ numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
 
 #include <vector>
 #include <numeric>
+#include <algorithm>
 
-class NumArray {
+class NumArray { // вариант - дерево отрезков в векторе
 public:
-    NumArray(std::vector<int>& nums) : data(nums) {
-        sumed.resize(data.size());
-        sumed[0] = data[0];
-        for (int i = 1; i < nums.size(); i++) {
-            sumed[i] = sumed[i - 1] + data[i];
-        }
+    NumArray(std::vector<int>& nums) : 
+        data(nums),
+        N(nums.size() - 1)
+    {
+        seg_tree.resize((N + 1) * 4);
+        buildSegTree(1, 0, N);
     }
 
     void update(int index, int val) {
-        int dif = val - data[index];
-        //data[index] = val;
-        mdif[index] = dif;
+        updateSegTree(1, 0, N, index, val);
     }
 
     int sumRange(int left, int right) {
-        //int sum = 0;
-        //for (int i = left; i <= right; i++) {
-        //    sum += data[i];
-        //}
-        //if (right - left < 1000) {
-        //    return std::accumulate(data.begin() + left, data.begin() + right + 1, 0);
-        //} 
-        //int mid = (right - left) / 2;
-        //future<int> f = std::async([this, left, right, mid] {return std::accumulate(this->data.begin() + left + mid + 1, this->data.begin() + right + 1, 0); });
-        //int sum = sumRange(left, mid);
-        //return sum + f.get();
-        int total_dif = 0;
-        if (!mdif.empty()) {
-            auto l_it = mdif.lower_bound(left);
-            auto r_it = mdif.upper_bound(right);
-            for (auto i = l_it; i != r_it; i++) {
-                total_dif += i->second;
-            }
-        }
-
-        if (left > 0) {
-            return sumed[right] - sumed[left - 1] + total_dif;
-        }
-        else {
-            return sumed[right] + total_dif;
-        }
+        return sumRequest(1, 0, N, left, right);
     }
 
 private:
-    std::vector<int> data, sumed;
-    std::map<int, int> mdif;
+    std::vector<int> data, seg_tree;
+    const int N;
+
+    void buildSegTree(int node_idx, int l_bound, int r_bound ) {
+        if (l_bound == r_bound) {
+            seg_tree[node_idx] = data[l_bound];
+        }
+        else {
+            int middle = (l_bound + r_bound) / 2;
+            buildSegTree(node_idx * 2, l_bound, middle);
+            buildSegTree(node_idx * 2 + 1, middle + 1, r_bound);
+            seg_tree[node_idx] = seg_tree[node_idx * 2] + seg_tree[node_idx * 2 + 1];
+        }
+    }
+
+    void updateSegTree(int node_idx, int l_bound, int r_bound, int pos, int new_val) {
+        if (l_bound == r_bound) {
+            seg_tree[node_idx] = new_val;
+        }
+        else {
+            int middle = (l_bound + r_bound) / 2;
+            if (pos <= middle) {
+                updateSegTree(node_idx * 2, l_bound, middle, pos, new_val);
+            }
+            else {
+                updateSegTree(node_idx * 2 + 1, middle + 1, r_bound, pos, new_val);
+            }
+            seg_tree[node_idx] = seg_tree[node_idx * 2] + seg_tree[node_idx * 2 + 1];
+        }
+    }
+
+    int sumRequest(int node_idx, int l_bound, int r_bound, int l, int r) {
+        if (l > r)
+            return 0;
+        if (l == l_bound && r == r_bound)
+            return seg_tree[node_idx];
+        int middle = (l_bound + r_bound) / 2;
+        return sumRequest(node_idx * 2    , l_bound   , middle , l                      , std::min(r, middle))
+             + sumRequest(node_idx * 2 + 1, middle + 1, r_bound, std::max(l, middle + 1), r                  );
+    }
 };
