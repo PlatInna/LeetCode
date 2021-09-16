4d8a039adf2c67f4546e1cd6541336e56d795f47
Plat <yarkinpy@gmail.com>
2021-09-15 19:09:51 -0500

    Binary Search v.1

--- LeetCode/BinarySearch.h
+++ LeetCode/BinarySearch.h
@@ -27,3 +27,19 @@ int search(std::vector<int>& nums, int target) {
 		return r;
 	}
 }
+
+int binarySearch(vector<int>& nums, int target) {
+	if (nums.size() == 0)
+		return -1;
+
+	int left = 0, right = nums.size() - 1;
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) { return mid; }
+		else if (nums[mid] < target) { left = mid + 1; }
+		else { right = mid - 1; }
+	}
+
+	// End Condition: left > right
+	return -1;
+}
