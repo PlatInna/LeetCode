637f9eaa62ddcdb4fc915d860cc0b28e3bb8af15
Plat <yarkinpy@gmail.com>
2021-09-18 18:53:52 -0500

    Binary Search, Template #2

--- LeetCode/BinarySearch.h
+++ LeetCode/BinarySearch.h
@@ -47,3 +47,24 @@ int binarySearch_1(std::vector<int>& nums, int target) {
 	return -1;
 }
 
+// Template #2 is an advanced form of Binary Search. It is used to search for an element or condition which requires accessing the current index 
+// and its immediate right neighbor's index in the array.
+// Using half - open interval [left, right)
+int binarySearch_2(std::vector<int>& nums, int target) {
+	if (nums.size() == 0)
+		return -1;
+
+	int left = 0, right = nums.size();
+	while (left < right) {
+		// Prevent (left + right) overflow
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) { return mid; }
+		else if (nums[mid] < target) { left = mid + 1; }
+		else { right = mid; }
+	}
+
+	// Post-processing:
+	// End Condition: left == right
+	if (left != nums.size() && nums[left] == target) return left;
+	return -1;
+}
