9e3ef46dd21427b2b1286b5b510e2884a971e00d
Plat <yarkinpy@gmail.com>
2021-12-08 17:35:56 -0600

    Stack

---
+++ LeetCode/MyStack.cpp
@@ -0,0 +1,41 @@
+#include "MyStack.h"
+
+stack_size_error::stack_size_error(const std::string& msg) : std::length_error(msg) 
+{};
+stack_bad_alloc::stack_bad_alloc(const std::string& msg) : std::bad_alloc() 
+{};
+stack_overflow::stack_overflow(const std::string& msg) : std::logic_error(msg) 
+{};
+stack_empty::stack_empty(const std::string& msg) : std::logic_error(msg) 
+{};
+
+Stack::Stack(int size) {
+	if (size <= 0)
+		throw stack_size_error("Stack size must be positive integer");
+	
+	try {
+		stackstore = new int[size];
+	}
+	catch(std::bad_alloc& ba) {
+		throw stack_bad_alloc("Stack allocation error");
+	}
+	
+	stacksize = size;
+	SP = 0;
+}
+
+Stack::~Stack() {
+	delete[] stackstore;
+}
+
+void Stack::Push(int value) {
+	if (SP == stacksize)
+		throw stack_overflow("Stack size exceeded");
+	stackstore[SP++] = value;
+}
+
+int Stack::Pop() {
+	if (SP == 0)
+		throw stack_empty("Stack is empty");
+	return stackstore[--SP];
+}
---
+++ LeetCode/MyStack.h
@@ -0,0 +1,42 @@
+//#pragma once
+
+#ifndef __MYSTACK__  // instead of pragma once
+#define __MYSTACK__
+
+#include<iostream>
+#include<exception>
+#include<stdexcept>
+
+class stack_size_error : public std::length_error {
+public:
+    explicit stack_size_error(const std::string& msg);
+};
+
+class stack_bad_alloc : public std::bad_alloc {
+public:
+    explicit stack_bad_alloc(const std::string& msg);
+};
+
+class stack_overflow : public std::logic_error {
+public:
+    explicit stack_overflow(const std::string& msg);
+};
+
+class stack_empty : public std::logic_error {
+public:
+    explicit stack_empty(const std::string& msg);
+};
+
+class Stack {
+private:
+    int* stackstore;
+    int stacksize;
+    int SP;
+public:
+    Stack(int size = 100);
+    ~Stack();
+    void Push(int value);
+    int Pop();
+};
+
+#endif
